99aa22c16502097523301aa851fe09d639fb2468
Luca D'Ettorre <ldettorre21@gmail.com>
2020-03-12 17:05:00 +0000

    Products Setup completed.

---
+++ 06-Products-Setup/Product-Details/products/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+# Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products' : products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
